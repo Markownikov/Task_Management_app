6a4f217e14058152fd66fb4b6edec1efdb39d2c2
Vishnu Shukla <vishnushuklac841@gmail.com>
2024-07-28 01:38:48 +0530

    Added new features and fixed bugs

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
     });
     console.log("MongoDB connected");
   } catch (error) {
